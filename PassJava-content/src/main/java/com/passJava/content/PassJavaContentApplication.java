0179ca521320274ea42d348a7ac14c1e6e30f448
hyz <hyz@qq.com>
2023-03-09 22:14:14 +0800

    第一次提交，完成项目搭建

---
+++ PassJava-content/src/main/java/com/passJava/content/PassJavaContentApplication.java
@@ -0,0 +1,13 @@
+package com.passJava.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassJavaContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassJavaContentApplication.class, args);
+    }
+
+}
---
+++ PassJava-content/src/test/java/com/passJava/content/PassJavaContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passJava.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassJavaContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
